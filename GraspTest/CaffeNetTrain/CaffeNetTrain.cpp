347a88619e36d9e772b7923d66ec1faa17745d10
yinsp398 <yinshaopeng398@163.com>
2018-03-21 23:31:10 +0800

    update CaffeNetTrain

--- GraspTest/CaffeNetTrain/CaffeNetTrain.cpp
+++ GraspTest/CaffeNetTrain/CaffeNetTrain.cpp
@@ -2,13 +2,20 @@
 #include "MergeMean.h"
 #include <stdio.h>
 
+unsigned int ImgCnt = 0;
+
 int main()
 {
+	//生成配置 solver.prototxt文件
+	std::string caffemodelfile;
+	//调用caffe训练数据
+	std::string TrainCmd = "caffe.exe train -solver solver.prototxt -weights " + caffemodelfile;
+	system(TrainCmd.c_str());
+	//是否阻塞？？
+	//Convert DB
 
-	convert_dataset("..\\ImageSet\\", "..\\ImageSet\\label.txt", "..\\ImageSet\\lmdb_test", "lmdb");
-	addto_dataset("..\\ImageSet\\", "2mmCrossLine.png", 1, "..\\ImageSet\\lmdb_test", "lmdb");
-	getimage_dataset("..\\ImageSet\\lmdb_test", "lmdb", "..\\ImageSet\\OutImg\\", "..\\ImageSet\\OutImg\\label.txt");
-	std::cin.get();
+	//Get MeanDB
+	
 
 	return 0;
 }
